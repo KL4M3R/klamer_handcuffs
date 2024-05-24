f399d40a256a0b1069735b7e0bcf2dea985fba31
klamer.dev <161390980+KL4M3R@users.noreply.github.com>
2024-05-24 18:03:02 +0200

    Update Config.lua

--- Config.lua
+++ Config.lua
@@ -1,6 +1,6 @@
 Config = {}
 
-Config.HandsUp = true -- true/false. do you want the function for raising hands on the key to work?
+Config.HandsUp = true -- true/false.  show of hands should be required
 Config.HandsUp_key = 'GRAVE' --  default is " ~ "   | https://docs.fivem.net/docs/game-references/input-mapper-parameter-ids/keyboard/
 
 Config.Notify = 'ox_lib' -- okokNotify, ox_lib | 
